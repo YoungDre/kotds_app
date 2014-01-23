ba81279f6cd28a77baebf3b3678faf451f81f4d0
' <'>
2014-01-23 15:02:30 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KotdsApp::Application.config.secret_key_base = 'c133c29a9cfcc17a0d7d3e8b33df03f299eddd1481574c983e1d95029ff4b46296bd3f7e42c35981127343efdb59fc81713697f4ddd7e67b51b4e2ce9d02e4c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KotdsApp::Application.config.session_store :cookie_store, key: '_kotds_app_session'
